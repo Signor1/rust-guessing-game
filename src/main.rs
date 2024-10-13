a79b2d66f76bf4e50b72b47b404f2bdd56346ea8
signor1 <emmanuelomemgboji@gmail.com>
2024-10-13 09:53:25 +0100

    feat: added color

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use colored::*;
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
@@ -7,6 +8,8 @@ fn main() {
 
     loop {
         let secret_number = rand::thread_rng().gen_range(1..=100);
+
+        println!("The secret number is: {}", secret_number);
 
         println!("Please input your guess.");
 
@@ -24,12 +27,12 @@ fn main() {
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small! The secret number is {}", secret_number),
-            Ordering::Greater => println!("Too big! The secret number is {}", secret_number),
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
             Ordering::Equal => {
                 println!(
-                    "You win! Your guess matched the secret number {}",
-                    secret_number
+                    "{}",
+                    "You win! Your guess matched the secret number".green(),
                 );
                 break;
             }
